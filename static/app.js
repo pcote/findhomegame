e47c806a2cc416d86200adee31fbd22aebd68460
Phil Cote <cotejrp@gmail.com>
2017-03-06 10:12:51 -0500

    Initial commit.

---
+++ static/app.js
@@ -0,0 +1,92 @@
+$(function(){
+
+    /************** Sprite Class ******************/
+    function Sprite(){
+        this.FRAME_WIDTH = 400;
+        this.FRAME_HEIGHT = 600;
+        this.DIRECTION_DOWN = 0;
+        this.DIRECTION_UP = 1;
+        this.DIRECTION_LEFT = 2;
+        this.DIRECTION_RIGHT = 3;
+
+        this.currentWalkFrame = -1;
+        this.MAX_WALK_FRAME = 3;
+        this.currentDirection = 0;
+        this.currentWalkFrame = 0;
+
+        this.turn = function(newDirection){
+            this.currentDirection = newDirection;
+        };
+
+        this.takeStep = function(){
+            this.currentWalkFrame += 1;
+            if(this.currentWalkFrame > this.MAX_WALK_FRAME){
+                this.currentWalkFrame = 0;
+            }
+        };
+
+
+        this.getOffsets = function(){
+            var offsets = {
+                walk: this.currentWalkFrame * this.FRAME_WIDTH,
+                direction: -this.currentDirection * this.FRAME_HEIGHT
+            };
+            return offsets;
+        };
+
+    }
+
+    /*********** Action Mappings for controller button presses. *********/
+    LEFT_BUTTON = 14;
+    RIGHT_BUTTON = 15;
+    UP_BUTTON = 12;
+    DOWN_BUTTON = 13;
+
+    var buttonActionMap = [];
+    buttonActionMap[LEFT_BUTTON] = function(){
+        boy.turn(boy.DIRECTION_LEFT);
+    };
+
+    buttonActionMap[RIGHT_BUTTON] = function(){
+        boy.turn(boy.DIRECTION_RIGHT);
+    };
+
+    buttonActionMap[UP_BUTTON] = function(){
+        boy.turn(boy.DIRECTION_UP);
+    };
+
+    buttonActionMap[DOWN_BUTTON] = function(){
+        boy.turn(boy.DIRECTION_DOWN);
+    };
+
+
+    /******** Interval Callback function to poll for button presses. ***********/
+    pollController = function(evt){
+        var gamepad = navigator.getGamepads()[0];
+        if(!gamepad){
+            return;
+        }
+
+        var i;
+        for(i=0; i<gamepad.buttons.length; i++){
+            var btn = gamepad.buttons[i];
+            if(btn.pressed){
+                buttonActionMap[i]();
+            }
+        }
+
+    };
+
+    /**************** Interval callback function for the animation walk cycle **********/
+    var animateWalk = function(evt){
+        boy.takeStep();
+        var offsets = boy.getOffsets();
+        var cssString = "url(sprite_sheet_boy.png) " + offsets.walk + "px " + offsets.direction + "px";
+        $(".boy").css("background", cssString);
+    };
+
+    var boy = new Sprite();
+    setInterval(pollController, 125);
+    setInterval(animateWalk, 250);
+
+});
